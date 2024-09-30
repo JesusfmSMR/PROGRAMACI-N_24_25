d6bf2074642326d17b4cf15e7a85173d177b6db1
diurno <diurno@192.168.0.126>
2024-09-30 09:33:14 +0200

    Primera subida de proyecto.

---
+++ CICLODAW2425/src/unidades/unidad1/ejemplos/Programa1.java
@@ -0,0 +1,11 @@
+package unidades.unidad1.ejemplos;
+
+public class Programa1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Mensaje");
+		
+	}
+
+}
